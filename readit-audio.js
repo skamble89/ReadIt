6c8a3409f0d5c978d2c817ca2eb5093db6a6280d
skamble1989 <saurabhkamble1989@gmail.com>
2015-04-19 22:51:19 +0530

    refactoring

--- readit-audio.js
+++ readit-audio.js
@@ -32,5 +32,3 @@
 
     Read(GetSelectedText());
 })();
-
-(function () { function GetSelectedText() { if (window.getSelection) { return window.getSelection().toString(); } else { if (document.selection.createRange) { return document.selection.createRange().text; } } } function Read(text) { var textArray = text.split(' '), index = 0, readWords = 10; if (textArray.length > 0) { (function ReadText(readText) { if (index < textArray.length) { var iframe = document.createElement('iframe'); iframe.style.display = 'none'; iframe.addEventListener('load', function () { var self = this; setTimeout(function () { index += readWords; document.body.removeChild(self); ReadText(textArray.slice(index, index + readWords).join(' ')); }, 5000); }); document.body.appendChild(iframe); iframe.src = 'https://translate.google.com/translate_tts?ie=UTF-8&q=' + encodeURI(readText) + '&tl=en&total=1&idx=0&textlen=' + readText.length + '&prev=input'; } })(textArray.slice(index, index + readWords).join(' ')) } } Read(GetSelectedText()); })();
